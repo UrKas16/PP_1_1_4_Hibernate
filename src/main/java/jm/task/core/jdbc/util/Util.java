6232f5736aa9c6c4e798d6065635b8978efcf8c6
Yurii <kasyurvik568@gmail.com>
2022-09-05 11:59:53 +0300

    pp_1_1_4_Hibernate without xml files

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,27 +15,29 @@ public class Util {
     private static final Environment environment = null;
 
     public static SessionFactory buildSessionFactory() {
-        try {
-            Configuration config = new Configuration();
-            Properties prop = new Properties();
-            prop.put(environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            prop.put(environment.URL, "jdbc:mysql://localhost:3306/pp_1_1_4_hibernate");
-            prop.put(environment.USER, "root");
-            prop.put(environment.PASS, "Iqprimer100@");
-            prop.put(environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-            prop.put(environment.SHOW_SQL, "true");
-            prop.put(environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        if (sessionFactory == null) {
+            try {
+                Configuration config = new Configuration();
+                Properties prop = new Properties();
+                prop.put(environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                prop.put(environment.URL, "jdbc:mysql://localhost:3306/pp_1_1_4_hibernate");
+                prop.put(environment.USER, "root");
+                prop.put(environment.PASS, "Iqprimer100@");
+                prop.put(environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                prop.put(environment.SHOW_SQL, "true");
+                prop.put(environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-            config.setProperties(prop);
-            config.addAnnotatedClass(User.class);
+                config.setProperties(prop);
+                config.addAnnotatedClass(User.class);
 
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(config.getProperties()).build();
-            sessionFactory = config.buildSessionFactory(serviceRegistry);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(config.getProperties()).build();
+                sessionFactory = config.buildSessionFactory(serviceRegistry);
 
-        } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
+            } catch (Throwable ex) {
+                System.err.println("Initial SessionFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
         }
         return sessionFactory;
     }
